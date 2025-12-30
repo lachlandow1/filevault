3cec09cdebb97ca924c6d803d485cb27aa0ef833
lachlandow1 <lachlan.dow@solirius.com>
2025-12-30 16:03:48 +0000

    fix(infra): update names to be consistent for pipeline builds (#4)

--- src/azure-sa/infra/compute.tf
+++ src/azure-sa/infra/compute.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_linux_web_app" "app" {
-  name                = "${var.app_name}-web-${random_string.suffix.result}"
+  name                = "${var.app_name}-web"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.asp.location
   service_plan_id     = azurerm_service_plan.asp.id
--- src/azure-sa/infra/container-registry.tf
+++ src/azure-sa/infra/container-registry.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${replace(var.app_name, "-", "")}acr${random_string.suffix.result}"
+  name                = "${replace(var.app_name, "-", "")}acr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
